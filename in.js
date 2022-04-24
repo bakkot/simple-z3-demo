3a87bc70b6235f9deb0663a8c85cd2696b9f2a0f
Kevin Gibbons <bakkot@gmail.com>
2022-04-23 23:39:10 -0700

    take two, with fork

--- in.js
+++ in.js
@@ -1,21 +1,26 @@
 'use strict';
 
-let { init } = require('z3-solver');
+let { init, Z3_error_code } = require('z3-solver/build/wrapper.js');
 
 let mod;
-window.run = async str => {
+async function run(str) {
   if (mod == null) {
-    mod = await init();
+    mod = await init(initZ3);
   }
   let { em, Z3 } = mod;
+  Z3.global_param_set('verbose', '10');
   let cfg = Z3.mk_config();
   let ctx = Z3.mk_context(cfg);
 
   try {
-    output.value = await Z3.eval_smtlib2_string(ctx, str);
-  } catch (e) {
-    output.value = `FAILED: ${e.message}`;
+    let out = await Z3.eval_smtlib2_string(ctx, str);
+    if (Z3.get_error_code(ctx) !== Z3_error_code.Z3_OK) {
+      throw new Error(Z3.get_error_msg(ctx, Z3.get_error_code(ctx)));
+    }
+    return out;
+  } finally {
+    Z3.del_context(ctx);
   }
-
-  Z3.del_context(ctx);
 };
+window.run = run;
+
